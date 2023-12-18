0cb267153335712c34cf14e7c423b03bf867f8ce
James Bowers <james@vizzly.co>
2023-12-18 11:50:22 +0000

    ensure dist folder exists

--- apps/in-browser-webcomponent/build.sh
+++ apps/in-browser-webcomponent/build.sh
@@ -1,5 +1,10 @@
 latestversion=$(npm view @vizzly/dashboard version)
 
+RUN mkdir -p /opt/app
+WORKDIR /opt/app
+
+mkdir -p ./dist
+
 cp www/index.html dist/index.html
 
 sed -i "s/{{ VERSION }}/$(latestversion)/g" dist/index.html
