7573ad500e2c6bfbd71402f6691ef4918f0433fa
James Bowers <james@vizzly.co>
2024-01-17 18:03:01 +0000

    trigger rebuild

--- apps/dynamic-remote-config/app.tsx
+++ apps/dynamic-remote-config/app.tsx
@@ -22,8 +22,8 @@ function App() {
               projectId: 'prj_64ad10208e024edda174aa0436ebf590',
               secureFilters: {},
               dataSetIds: '*',
-              userReference: window.location.hash || 'a user id - 2874924832',
-              scope: 'read_write'
+              scope: 'read_write',
+              userReference: window.location.hash || 'a user id - 2874924832'
             })
           });
 
