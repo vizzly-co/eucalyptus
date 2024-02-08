1d9dc64b94cefd2bda46189429899d2519f6d1f5
James Bowers <james@vizzly.co>
2024-02-08 09:10:10 +0000

    sql views example app

---
+++ apps/sql-views/app.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import Vizzly from "@vizzly/dashboard";
+import fetch from 'isomorphic-fetch'
+
+function App() {
+  return (
+    <Vizzly.Dashboard
+        vizzlyApiHost="https://staging.api.vizzly.co"
+        // Hit dynamic query engine for remote configs
+        queryEngineEndpoint="https://staging.api.vizzly.co/managed/mqe_2366962f5f354df3a6fce9ac1b68eb47"
+        parentDashboardId="dsh_08d75fc72c4e4bcf970ce90d234f926d"
+        identity={async () => {
+          // Hit the auth app
+          const response = await fetch('http://koala-tree.vizzly.co:9012/identity', {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+            },
+            body: JSON.stringify({
+              projectId: 'prj_719ed637834e4605be0c19fe25a38ed5',
+              secureFilters: {},
+              dataSetIds: '*',
+              scope: 'read_write',
+              userReference: window.location.hash || 'a user id - 2874924832'
+            })
+          });
+
+          const { accessTokens } = await response.json();
+
+          return accessTokens;
+        }}
+      />
+  );
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
