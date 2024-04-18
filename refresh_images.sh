4e3ab55b63305c3de7776f068b4cd7213bc0c2f1
James Bowers <james@vizzly.co>
2024-04-18 11:27:21 +0100

    stop all containers, remove everything and then pull

--- refresh_images.sh
+++ refresh_images.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Remove old images
-docker system prune --force &
-
 # Define the prefix of images to update and new tag (generally the commit sha of this repo)
 PREFIX="ghcr.io/vizzly-co"
 NEW_TAG="$1"
@@ -16,7 +13,13 @@ fi
 # List Docker images and filter by prefix
 IMAGES=$(docker images --format '{{.Repository}}' | grep "^$PREFIX")
 
-# Loop through each image and update its tag
+# Stop all containers
+docker stop $(docker ps -a -q)
+
+# Remove all images
+docker rm $(docker ps -a -q)
+
+# Loop through each image and pull the given sha latest
 for IMAGE in $IMAGES; do
   docker pull "$IMAGE:$NEW_TAG" &
 done
