60e4e687c03d5b9142c5308a14c7f88d5ff1284f
James Bowers <james@vizzly.co>
2024-05-21 10:16:16 +0100

    add qe ecs service infra for remote-config-endpoint and WIP barebones of dashboard app for it

---
+++ apps/dashboard-remote-config-with-auth/app.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import Vizzly from "@vizzly/dashboard";
+import fetch from 'isomorphic-fetch'
+
+function App() {
+  return (
+    <Vizzly.Dashboard
+        vizzlyApiHost="https://staging.api.vizzly.co"
+        // Hit dynamic query engine for remote configs
+        queryEngineEndpoint={{
+          endpoint: "",
+          customHeaders: () => ({
+            'Auth-Remote-Config-Token': '1'
+          })
+        }}
+        parentDashboardId="dsh_78f5b281ae614344ad658cc885a127a6"
+        identity={async () => {
+          // Hit the auth app
+          const response = await fetch('http://koala-tree.vizzly.co:9012/identity', {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+            },
+            body: JSON.stringify({
+              projectId: 'prj_ccb21c52e567489bb776b4fc2b6cfa3c',
+              secureFilters: {},
+              dataSetIds: '*',
+              scope: 'read_write',
+              userReference: window.location.hash || 'a user id - 2874924832'
+            })
+          });
+
+          const { accessTokens } = await response.json();
+
+          return accessTokens;
+        }}
+      />
+  );
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
