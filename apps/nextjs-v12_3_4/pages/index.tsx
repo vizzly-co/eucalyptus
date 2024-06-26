db3f538cf7c7c5714d5a954b24b440733e7f3098
Harry Ronchetti <harryronchetti@gmail.com>
2024-06-26 18:58:46 +0100

    removing custom server

--- apps/nextjs-v12_3_4/pages/index.tsx
+++ apps/nextjs-v12_3_4/pages/index.tsx
@@ -30,15 +30,15 @@ export function getIdentity(userReference: string) {
 const Home: NextPage = () => {
   return (
     <div>
-    <Vizzly.Dashboard
-    id="dashboard-id"
-    featureToggles={{ disableAutoSave: true }}
-    vizzlyApiHost="https://staging.api.vizzly.co"
-    queryEngineEndpoint="https://staging.api.vizzly.co/managed/mqe_f69a3d3ad797479492e2f91a25d3add2"
-    parentDashboardId="dsh_0c23c9f4146745118fcfaabf5aab5184"
-    identity={getIdentity("nextjs-v12.3.4")}
-  />
-  </div>
+      <Vizzly.Dashboard
+        id="dashboard-id"
+        featureToggles={{ disableAutoSave: true }}
+        vizzlyApiHost="https://staging.api.vizzly.co"
+        queryEngineEndpoint="https://staging.api.vizzly.co/managed/mqe_f69a3d3ad797479492e2f91a25d3add2"
+        parentDashboardId="dsh_0c23c9f4146745118fcfaabf5aab5184"
+        identity={getIdentity("nextjs-v12.3.4")}
+      />
+    </div>
   )
 }
 
